ee4bd75cd8390f3fd169fd810d4782eabff0fbec
biel-365 <aroiogabriel@gmail.com>
2025-12-04 14:27:27 -0300

    Adiciona página "Processos" e atualiza navegação
    Foi criada a nova página "Processos" (`Processos.cshtml` e
    `Processos.cshtml.cs`), substituindo o conteúdo da página
    "Privacy". A nova página detalha o processo de descarte de
    resíduos, com design responsivo utilizando Bootstrap.
    
    Alterações adicionais:
    - Adicionada nova entrada no menu de navegação em `_Layout.cshtml`.
    - Criada a ação `Processos` no `HomeController` para servir a nova
      página.
    - Atualizado o arquivo de solução `ProjetoEcoCycle.sln` para
      compatibilidade com a versão 17.12.35707.178.
    - Removido o conteúdo da página `Privacy.cshtml`.
    
    Essas mudanças melhoram a organização do projeto e oferecem uma
    experiência mais rica ao usuário.

--- ProjetoEcoCycle/Controllers/HomeController.cs
+++ ProjetoEcoCycle/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace ProjetoEcoCycle.Controllers
             return View();
         }
 
+        public IActionResult Processos()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ ProjetoEcoCycle/Views/Home/Processos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjetoEcoCycle.Views.Home
+{
+    public class ProcessosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
